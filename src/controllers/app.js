076d0b01043d3fb07b43d6c84ee3843caedf0e10
paulo <paulowilliancassel@gmail.com>
2023-12-04 19:30:55 -0300

    funcionando

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const server = express();
 const dados = require('./data/dadosClientes.json');
-const clientesRouter = require('../controllers/controllerClientes');
+const clientesRouter = require('./controllerClientes.js');
 const cors = require('cors');
 const fs = require('fs');
 
 server.use(express.json());
 server.use(cors());
 
-server.use('/api', clientesRouter);
+server.use('/api', clientesRouter.server);
 
 server.listen(3000, () => {
     console.log('O servidor está funcionando! :D');
